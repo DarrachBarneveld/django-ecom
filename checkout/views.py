1578f0a941cec437b2b107916c1d45dbd70526ee
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-11-15 22:11:04 +0000

    Added basic stripe payment

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key':'pk_test_51OCqypBdKna3LSPr9mkDk2sQO9VS4DiQlFsWchNXRfVSYxEUteVrYrwD9uHg0a5puGwgnLYuzmaq3N64jxJzXGux00U63GNTA9',
+        "client_secret": 'test_client_secret'
     }
     return render(request, template, context)
